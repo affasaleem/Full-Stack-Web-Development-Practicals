c59856e964f73e8166ebe4ee121ef414ef550d01
affasaleem <affasaleem.it@gmail.com>
2026-06-16 06:37:41 +0530

    my task folder updated

---
+++ JavaScript Practicals/Section-19/index.js
@@ -0,0 +1,18 @@
+
+// $("h1"); //is equal to document.querySelector("h1"); or document.querySelectorAll("h1");
+
+// $("h1").css("color", "green"); // css changes
+
+// $("h1").addClass("big-title"); // add css class we can add by one >> ("big-title margin-50")
+
+// $("h1").text("Bye"); // text edit
+
+// $("button").text("<em>Hey</em>");
+
+// $("button").html("<em>Hey</em>"); //usinghtml elements inside
+
+//attrupte changes
+
+//$("a").attr("href", "https://www.yahoo.com/"); //changing url
+
+$("h1").click (function())
